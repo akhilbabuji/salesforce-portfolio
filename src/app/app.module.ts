ff3a3642872325e31fb45b221b0594d0cc079767
Anusree Basanth <anusreebasanth@Anusrees-MacBook-Air.local>
2026-02-04 00:24:35 +0530

    feat: use HashLocationStrategy for GitHub Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule],
+  providers: [{ provide: LocationStrategy, useClass: HashLocationStrategy }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
